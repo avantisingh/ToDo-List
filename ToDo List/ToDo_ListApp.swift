c4bef041596af8ee74ddf98fcdc9a1906a3687b1
Avanti Singh <avantisingh19@gmail.com>
2023-06-13 14:52:29 -0500

    Initial Commit

---
+++ ToDo List/ToDo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_ListApp.swift
+//  ToDo List
+//
+//  Created by scholar on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
