c228e8adaea76cb26fcb8e2f65082342987a7e30
Avanti Singh <avantisingh19@gmail.com>
2023-06-13 15:23:28 -0500

    complete iteration 0

--- ToDo List/ContentView.swift
+++ ToDo List/ContentView.swift
@@ -10,12 +10,22 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Text("To-Do List")
+                    .font(.system(size: 40))
+                         .fontWeight(.black)
+                Spacer()
+            }
+            .padding()
+            
+            Button(action: {
+                                
+            }) {
+            Text("+")
+            }
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by scholar on 6/13/23.
+//
+
+import Foundation
+class ToDoItem {
+    var title : String
+    var isImportant : Bool
+    
+    init(title: String, isImportant: Bool = false) {
+        self.title = title
+        self.isImportant = isImportant
+            }
+        }
